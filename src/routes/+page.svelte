0c01b1bcba718bfde09c131cfa40fef82aadb7c4
Ed Sunman <ed.sunman@freshlabel.co.uk>
2024-03-19 22:42:07 +0000

    github link

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,29 @@
 <script lang="ts">
-  import App from '$lib/components/App.svelte'
+	import App from '$lib/components/App.svelte';
 </script>
 
 <div>
-  <App />
+	<a href="https://github.com/edsunman/threlte-particles" target="_blank">github</a>
+	<App />
 </div>
 
 <style>
-  :global(body) {
-    margin: 0;
-  }
+	:global(body) {
+		margin: 0;
+	}
 
-  div {
-    width: 100vw;
-    height: 100vh;
-    background: rgb(13, 19, 32);
-    background: linear-gradient(180deg, rgba(13, 19, 32, 1) 0%, rgba(8, 12, 21, 1) 100%);
-  }
+	div {
+		width: 100vw;
+		height: 100vh;
+		background: rgb(13, 19, 32);
+		background: linear-gradient(180deg, rgba(13, 19, 32, 1) 0%, rgba(8, 12, 21, 1) 100%);
+	}
+
+	a {
+		position: absolute;
+		color: white;
+		text-decoration: none;
+		margin: 30px;
+		font-family: monospace;
+	}
 </style>
